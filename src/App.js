baff227cf48f3e2abf8e4df3ac43e865504bfd88
soni.niket29@gmail.com <nikz@0133>
2020-05-25 15:52:57 +0530

    updated our menu item component with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from "react";
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
+
+const HatsPage = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>{props.location.pathname} Page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={HatsPage} />
+        <Route path="/shop/jackets" component={HatsPage} />
+        <Route path="/shop/sneakers" component={HatsPage} />
+        <Route path="/shop/womens" component={HatsPage} />
+        <Route path="/shop/mens" component={HatsPage} />
+      </Switch>
     </div>
   );
 }
