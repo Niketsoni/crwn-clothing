b1ec40e902841df8c22b4a679633033e5072bbac
soni.niket29@gmail.com <nikz@0133>
2020-06-03 02:03:45 +0530

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { auth, createUserProfileDocument } from "./firebase/firebase.util";
 
 import CheckoutPage from "./pages/checkout/checkout.component";
 import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./pages/shop/shope.component";
+import ShopPage from "./pages/shop/shop.component";
 import SignInSignUp from "./pages/sign-in-sign-up/sign-in-sign-up.component";
 import Header from "./components/header/header.component";
 import "./App.css";
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,26 @@
 // it'll work as a reducer function to combine all other states
 
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // type of storage(here we have imported localStorage )
+
 import userReducer from "./user/user.reducer";
 import cartReducer from "./cart/cart.reducer";
+import directoryReducer from "./directory/directory.reducer";
+import shopReducer from "./shop/shop.reducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage, // default storage type
+  whiteList: ["cart"], // reducer which we want to persist
+};
+
+const rootReducer = combineReducers({
   // it'll help use to combine all kind of different reducers
   user: userReducer,
   cart: cartReducer,
+  directory: directoryReducer,
+  shop: shopReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
